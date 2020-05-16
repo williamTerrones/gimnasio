237207696cff27590a8d9ea5d43be2225394cec6
Wildragonblack <https://honeymustard.mx:2096/>
2020-05-16 00:48:34 -0500

    [Add] calcular fechas en inscripciones con base al precio seleccionado

--- src/app/admin/inscripcion/inscripcion.component.ts
+++ src/app/admin/inscripcion/inscripcion.component.ts
@@ -2,6 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { Inscripcion } from 'src/app/models/inscripccion';
 import { ClienteService } from 'src/app/services/cliente.service';
 import { PrecioService } from 'src/app/services/precio.service';
+import * as moment from 'moment';
+import Base = moment.unitOfTime.Base
+
+interface FechaI {
+  id:number,
+  total:number,
+  substract:string,
+}
 
 @Component({
   selector: 'app-inscripcion',
@@ -12,7 +20,16 @@ export class InscripcionComponent implements OnInit {
 
   public inscripcion:Inscripcion = new Inscripcion;
 
-  constructor(public clienteService:ClienteService, public precioService:PrecioService) { }
+  public fechas:Array<FechaI> = new Array();
+
+  constructor(public clienteService:ClienteService, public precioService:PrecioService) {
+    this.fechas.push({id:0,total:1,substract:"days"})
+    this.fechas.push({id:1,total:7,substract:"days"})
+    this.fechas.push({id:2,total:15,substract:"days"})
+    this.fechas.push({id:3,total:1,substract:"months"})
+    this.fechas.push({id:4,total:1,substract:"years"})
+    this.inscripcion.fecha_inicial = new Date()
+   }
 
   ngOnInit(): void {
   }
@@ -30,6 +47,44 @@ export class InscripcionComponent implements OnInit {
     let precio = this.precioService.precios.find(precio => precio.id===idPrecio)
 
     this.inscripcion.precio = precio ? precio.ref : null;
+    
+    this.calcularFechaFinal();
+    
+  }
+
+  asignarFechaInicial(fecha:string){
+    this.inscripcion.fecha_inicial = fecha!=='' ? new Date(fecha) : null;
+  }
+
+  calcularFechaFinal(){
+    
+    if(this.inscripcion.precio===null || this.inscripcion.fecha_inicial===null){
+      this.inscripcion.fecha_final = null;
+      return false;
+    }
+
+    const precio = this.precioService.precios.find(precio => precio.id===this.inscripcion.precio.id)
+
+    if(precio){
+
+      console.log(precio)
+      console.log(this.fechas)
+      
+      const tiempo = this.fechas.find(fecha => String(fecha.id)===String(precio.tipo_duracion))
+      if(tiempo){
+        console.log("Tiempo encontrado ", tiempo)
+        let fecha_inicial = moment(this.inscripcion.fecha_inicial, "DD-MM-YYYY")
+        let fecha_final = fecha_inicial.add(precio.duracion*tiempo.total,tiempo.substract as Base)
+        console.log("Fecha final ", fecha_final)
+        console.log("asd ", fecha_final.format("MM/DD/YYYY"))
+        this.inscripcion.fecha_final = new Date(fecha_final.format("MM/DD/YYYY"));
+        return true;
+      }
+
+    }
+
+    this.inscripcion.fecha_final = null;
+
     
   }
 
